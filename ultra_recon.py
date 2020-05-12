80bf410d5642f0f34db96cc08338a97d8fb979b8
sneakerhax <sneakerhax@gmail.com>
2020-05-11 22:39:44 -0700

    Added handling for non-existent Dockerfile path

--- ultra_recon.py
+++ ultra_recon.py
@@ -34,31 +34,35 @@ def main():
     censys_API_ID = config['censys.io']['censys_API_ID']
     censys_secret = config['censys.io']['censys_secret']
 
-    # Target output file check and creation
-    output_dir = Path('output', args.name)
-    output_dir.mkdir(exist_ok=True, parents=True)
-
     # Tool file
     tool_dir = Path('tools', args.image)
 
     # Build Docker image with Docker SDK for Python
     client = docker.from_env()
     print("[+] Building image " + str(args.image))
-    client.images.build(path=str(tool_dir), tag=args.image)
+    if Path.exists(tool_dir):
+        client.images.build(path=str(tool_dir), tag=args.image)
+    else:
+        print("[-] Path to Dockerfile does not exist")
+        exit()
+
+    # Target output file check and creation
+    output_dir = Path('output', args.name)
+    output_dir.mkdir(exist_ok=True, parents=True)
 
     # Run Docker container with Docker SDK for Python
     print("[+] Starting container " + str(args.image))
     target = args.target
     if args.image == "nmap":
-        o = client.containers.run(args.image, command=target + " -p 80")
+        container_output = client.containers.run(args.image, command=target + " -p 80")
     if args.image == "pydnsrecon":
-        o = client.containers.run(args.image, command=target, environment=["censys_API_ID=" + censys_API_ID, "censys_secret=" + censys_secret])
+        container_output = client.containers.run(args.image, command=target, environment=["censys_API_ID=" + censys_API_ID, "censys_secret=" + censys_secret])
 
     # Output container stdout to output folder
     outputpath = Path(output_dir, args.image + ".txt")
     print("[+] writing output to " + str(outputpath))
     with open(outputpath, 'w') as out:
-        out.write(o.decode())
+        out.write(container_output.decode())
 
 
 if __name__ == '__main__':
